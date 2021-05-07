71c53a07060e4c2250a3244d87387d8fd5532239
Aleksander Alekseev <mail@eax.me>
2021-05-07 15:47:53 +0300

    fix

--- src/ts_date_trunc.c
+++ src/ts_date_trunc.c
@@ -26,6 +26,34 @@ ts_date_trunc(PG_FUNCTION_ARGS)
 	//Timestamp timestamp, result;
 	//int64 period = -1;
 
+	if (PG_NARGS() > 2)
+	{
+	//if (PG_NARGS() > 2)
+	//	origin = DatumGetTimestamp(DirectFunctionCall1(date_timestamp, PG_GETARG_DATUM(2)));
+		ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("`origin` argument is not supported yet")));
+	}
+
+	if(interval->time != 0)
+	{
+		ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("only months are supported at the moment")));
+	}
+
+	if(interval->day != 0)
+	{
+		ereport(ERROR,
+			(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+			 errmsg("only months are supported at the moment")));
+	}
+
+	// DateADT is number of days (integer) since 2000-01-01
+	// 7752 == 2021-03-23
+	printf("interval->month = %d\n", interval->month);
+	printf("date = %d\n", date); 
+
 	if (DATE_NOT_FINITE(date))
 		PG_RETURN_DATEADT(date);
 
@@ -38,8 +66,7 @@ ts_date_trunc(PG_FUNCTION_ARGS)
 
 	/* convert to timestamp (NOT tz), bucket, convert back to date */
 	//timestamp = DatumGetTimestamp(DirectFunctionCall1(date_timestamp, PG_GETARG_DATUM(1)));
-	//if (PG_NARGS() > 2)
-	//	origin = DatumGetTimestamp(DirectFunctionCall1(date_timestamp, PG_GETARG_DATUM(2)));
+
 
 	//Assert(!TIMESTAMP_NOT_FINITE(timestamp));
 
