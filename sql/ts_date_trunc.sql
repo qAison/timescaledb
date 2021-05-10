1c0112c382deddbf9cc63468464bf62d4533f6ba
Aleksander Alekseev <mail@eax.me>
2021-05-10 14:41:08 +0300

    Fix

--- sql/ts_date_trunc.sql
+++ sql/ts_date_trunc.sql
@@ -2,7 +2,7 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
--- ts_date_trunc() is similar to date_trunc() in PostgreSQL but supports intervals like 'N month'
+-- ts_date_trunc() is similar to date_trunc() in PostgreSQL but supports intervals like 'N months'
 CREATE OR REPLACE FUNCTION ts_date_trunc(bucket_width INTERVAL, ts DATE) RETURNS DATE
 	AS '@MODULE_PATHNAME@', 'ts_date_trunc' LANGUAGE C IMMUTABLE PARALLEL SAFE STRICT;
 
