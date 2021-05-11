7bd30b39d1f257840ae155d3acea5b1bcb55473d
Aleksander Alekseev <mail@eax.me>
2021-05-11 13:22:37 +0300

    Fix

--- src/ts_date_trunc.h
+++ src/ts_date_trunc.h
@@ -6,13 +6,18 @@
 #ifndef TIMESCALEDB_DATE_TRUNC_H
 #define TIMESCALEDB_DATE_TRUNC_H
 
-#include <postgres.h>
 #include <fmgr.h>
+#include <postgres.h>
 
 #include "export.h"
 
-extern TSDLLEXPORT Datum ts_date_trunc(PG_FUNCTION_ARGS);
-extern TSDLLEXPORT Datum ts_date_trunc_timestamp(PG_FUNCTION_ARGS);
-extern TSDLLEXPORT Datum ts_date_trunc_timestamptz(PG_FUNCTION_ARGS);
+extern TSDLLEXPORT Datum
+ts_date_trunc(PG_FUNCTION_ARGS);
+
+extern TSDLLEXPORT Datum
+ts_date_trunc_timestamp(PG_FUNCTION_ARGS);
+
+extern TSDLLEXPORT Datum
+ts_date_trunc_timestamptz(PG_FUNCTION_ARGS);
 
 #endif /* TIMESCALEDB_DATE_TRUNC_H */
